8da12aa16cc7b493da4c86cd62b23a048c7f759c
Григоров Александр Дмитриевич <agrigorov@ct.cnt.ru>
2015-02-11 17:18:10 +0300

    Исправление webassets.

--- landing/assets.py
+++ landing/assets.py
@@ -1,6 +1,6 @@
 from django_assets import Bundle, register
 
-js = Bundle('js/jquery.min.js','js/jquery.dropotron.min.js','js/jquery.scrolly.min.js','js/skel.min.js','js/skel-layers.min.js','js/init.js', filters='jsmin', output='all.js')
+js = Bundle('js/jquery.min.js','js/jquery.dropotron.min.js','js/jquery.scrolly.min.js','js/jquery.onvisible.min.js','js/skel.min.js','js/skel-layers.min.js','js/init.js', filters='yui_js', output='all.js')
 register('js_all', js)
 
 css_noscipt = Bundle('css/skel.css','css/style.css','css/style-noscript.css', filters='cssmin', output='css-noscipt.css')
