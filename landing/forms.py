08df90ab982fe576806846b0239903276c52c661
Григоров Александр Дмитриевич <agrigorov@ct.cnt.ru>
2015-02-18 17:44:59 +0300

    исправление формы заказа

--- landing/forms.py
+++ landing/forms.py
@@ -5,7 +5,8 @@ from .models import Toys, Order
 class OrderForm(forms.ModelForm):
     price = forms.IntegerField(widget=forms.HiddenInput)
     toy_id = forms.IntegerField(widget=forms.HiddenInput)
-    mob = forms.CharField(label="Телефон",widget=forms.TextInput(attrs={'type':'tel','placeholder':'+79051234567'}),max_length=12,help_text="Укажите номер телефона. Наш менеджер с вами свяжиться.")
+    email = forms.EmailField(label="Email",widget=forms.EmailInput(attrs={'placeholder':'Email:olya@mail.ru'}))
+    mob = forms.CharField(label="+79051234567",widget=forms.TextInput(attrs={'placeholder':'+79051234567','type':'tel'}),max_length=12,help_text="Укажите номер телефона. Наш менеджер с вами свяжиться.")
     text = forms.CharField(label="Комментарий",widget=forms.Textarea(attrs={'cols':'5','rows':'5','placeholder':'Станция метро: Охотный ряд, с 9 до 18 часов'}),max_length=200,help_text="Укажите комментарий к заказу, станция метро где удобно было бы забрать, время в которое удобно было бы вам.")
     def save(self):
         data = self.cleaned_data
@@ -13,6 +14,27 @@ class OrderForm(forms.ModelForm):
         instance = Order(**data)
         instance.toy_id = toy.id
         instance.save()
+    def __init__(self, *args, **kwargs):
+        super(OrderForm, self).__init__(*args, **kwargs)
+        for field_name in self.fields:
+            field = self.fields.get(field_name)
+            if field:
+                if type(field.widget) in (forms.TextInput, ):
+                    field.widget.attrs.update(
+                        {'placeholder': field.label, 'class': 'span10'}
+                    )
+                if type(field.widget) in (forms.Select, ):
+                    field.widget.attrs.update({'class': 'span10'})
+                    field.empty_label = field.label
+
+    def as_p(self):
+        return self._html_output(
+            normal_row='<p%(html_class_attr)s>%(field)s%(help_text)s</p>',
+            error_row='%s',
+            row_ender='</p>',
+            help_text_html=' <span class="helptext">%s</span>',
+            errors_on_separate_row=True
+        )
 
     class Meta:
         # Provide an association between the ModelForm and a model
